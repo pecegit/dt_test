5ded0d7496400c91c0c0111ba785af61b05bb124
Robert Ropotan <rob@masterofmalt.com>
2017-02-03 01:44:12 +0000

    Filter module Hooks the "change" event for the controls on the filter panel and returns filters object

---
+++ js/filters_module.js
@@ -0,0 +1,40 @@
+var filtersModule = (function () {
+    var _typeFilterControls,
+        _speedFilterControl;
+
+    function init() {
+        _typeFilterControls = $('input[name=types_filter]').change(onChange);
+        _speedFilterControl = $('#speed_filter').change(onChange);
+    }
+
+    function onChange() {
+        PubSub.pub('filters_changed', getFilters());
+    }
+
+    function getFilters() {
+        var filters = {},
+            speed = _speedFilterControl.val();
+
+        filters["types"] = typeFiltersToArray();
+
+        if (speed.indexOf('GB') != -1) { //eh
+            filters["data"] = parseInt(speed, 10)
+        } else {
+            if (speed > 0)
+                filters["speed"] = speed;
+        }
+
+        return filters;
+    }
+
+    function typeFiltersToArray() {
+        return _typeFilterControls.map(function(){
+            return this.checked ? this.value : null
+        }).get();
+    }
+
+    return {
+        setup: init,
+        getFilters: getFilters
+    };
+})();
